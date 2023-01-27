6845d0167320aa280cbc340abc542ea38ca70142
Matthias <webmaster@pottgames.de>
2023-01-27 01:32:19 +0100

    Opcodes++
    Signed-off-by: Matthias <webmaster@pottgames.de>

--- src/gameboy/cpu/instructions/rlc.rs
+++ src/gameboy/cpu/instructions/rlc.rs
@@ -4,6 +4,105 @@ use crate::gameboy::memory::memory::Memory;
 use crate::gameboy::memory::mmu::Mmu;
 
 impl Cpu {
+    pub fn rl_b(&mut self) -> isize {
+        let msb = self.register.b >> 7;
+        self.register.b = (self.register.b << 1) | (self.register.is_flag_set(FlagId::C) as u8);
+
+        self.register.set_flag(FlagId::C, msb == 1);
+        self.register.set_flag(FlagId::Z, self.register.b == 0);
+        self.register.set_flag(FlagId::H, false);
+        self.register.set_flag(FlagId::N, false);
+
+        8
+    }
+
+    pub fn rl_c(&mut self) -> isize {
+        let msb = self.register.c >> 7;
+        self.register.c = (self.register.c << 1) | (self.register.is_flag_set(FlagId::C) as u8);
+
+        self.register.set_flag(FlagId::C, msb == 1);
+        self.register.set_flag(FlagId::Z, self.register.c == 0);
+        self.register.set_flag(FlagId::H, false);
+        self.register.set_flag(FlagId::N, false);
+
+        8
+    }
+
+    pub fn rl_d(&mut self) -> isize {
+        let msb = self.register.d >> 7;
+        self.register.d = (self.register.d << 1) | (self.register.is_flag_set(FlagId::C) as u8);
+
+        self.register.set_flag(FlagId::C, msb == 1);
+        self.register.set_flag(FlagId::Z, self.register.d == 0);
+        self.register.set_flag(FlagId::H, false);
+        self.register.set_flag(FlagId::N, false);
+
+        8
+    }
+
+    pub fn rl_e(&mut self) -> isize {
+        let msb = self.register.e >> 7;
+        self.register.e = (self.register.e << 1) | (self.register.is_flag_set(FlagId::C) as u8);
+
+        self.register.set_flag(FlagId::C, msb == 1);
+        self.register.set_flag(FlagId::Z, self.register.e == 0);
+        self.register.set_flag(FlagId::H, false);
+        self.register.set_flag(FlagId::N, false);
+
+        8
+    }
+
+    pub fn rl_h(&mut self) -> isize {
+        let msb = self.register.h >> 7;
+        self.register.h = (self.register.h << 1) | (self.register.is_flag_set(FlagId::C) as u8);
+
+        self.register.set_flag(FlagId::C, msb == 1);
+        self.register.set_flag(FlagId::Z, self.register.h == 0);
+        self.register.set_flag(FlagId::H, false);
+        self.register.set_flag(FlagId::N, false);
+
+        8
+    }
+
+    pub fn rl_l(&mut self) -> isize {
+        let msb = self.register.l >> 7;
+        self.register.l = (self.register.l << 1) | (self.register.is_flag_set(FlagId::C) as u8);
+
+        self.register.set_flag(FlagId::C, msb == 1);
+        self.register.set_flag(FlagId::Z, self.register.l == 0);
+        self.register.set_flag(FlagId::H, false);
+        self.register.set_flag(FlagId::N, false);
+
+        8
+    }
+
+    pub fn rl_hl(&mut self, mmu: &mut Mmu) -> isize {
+        let address = self.register.get_hl();
+        let old_value = mmu.read_byte(address);
+        let msb = old_value >> 7;
+        let new_value = (old_value << 1) | (self.register.is_flag_set(FlagId::C) as u8);
+        mmu.write_byte(address, new_value);
+
+        self.register.set_flag(FlagId::C, msb == 1);
+        self.register.set_flag(FlagId::Z, new_value == 0);
+        self.register.set_flag(FlagId::H, false);
+        self.register.set_flag(FlagId::N, false);
+
+        16
+    }
+
+    pub fn rl_a(&mut self) -> isize {
+        let msb = self.register.a >> 7;
+        self.register.a = (self.register.a << 1) | (self.register.is_flag_set(FlagId::C) as u8);
+
+        self.register.set_flag(FlagId::C, msb == 1);
+        self.register.set_flag(FlagId::Z, self.register.a == 0);
+        self.register.set_flag(FlagId::H, false);
+        self.register.set_flag(FlagId::N, false);
+
+        8
+    }
+
     pub fn rlc_a(&mut self) -> isize {
         let a = self.register.a;
         self.register.a = a.rotate_left(1);
--- src/gameboy/cpu/instructions/rrc.rs
+++ src/gameboy/cpu/instructions/rrc.rs
@@ -4,6 +4,113 @@ use crate::gameboy::memory::memory::Memory;
 use crate::gameboy::memory::mmu::Mmu;
 
 impl Cpu {
+    pub fn rr_b(&mut self) -> isize {
+        let lsb = self.register.b & 0x1;
+        let carry = (self.register.is_flag_set(FlagId::C) as u8) << 7;
+        self.register.b = (self.register.b >> 1) | carry;
+
+        self.register.set_flag(FlagId::Z, self.register.b == 0);
+        self.register.set_flag(FlagId::N, false);
+        self.register.set_flag(FlagId::H, false);
+        self.register.set_flag(FlagId::C, lsb != 0);
+
+        8
+    }
+
+    pub fn rr_c(&mut self) -> isize {
+        let lsb = self.register.c & 0x1;
+        let carry = (self.register.is_flag_set(FlagId::C) as u8) << 7;
+        self.register.c = (self.register.c >> 1) | carry;
+
+        self.register.set_flag(FlagId::Z, self.register.c == 0);
+        self.register.set_flag(FlagId::N, false);
+        self.register.set_flag(FlagId::H, false);
+        self.register.set_flag(FlagId::C, lsb != 0);
+
+        8
+    }
+
+    pub fn rr_d(&mut self) -> isize {
+        let lsb = self.register.d & 0x1;
+        let carry = (self.register.is_flag_set(FlagId::C) as u8) << 7;
+        self.register.d = (self.register.d >> 1) | carry;
+
+        self.register.set_flag(FlagId::Z, self.register.d == 0);
+        self.register.set_flag(FlagId::N, false);
+        self.register.set_flag(FlagId::H, false);
+        self.register.set_flag(FlagId::C, lsb != 0);
+
+        8
+    }
+
+    pub fn rr_e(&mut self) -> isize {
+        let lsb = self.register.e & 0x1;
+        let carry = (self.register.is_flag_set(FlagId::C) as u8) << 7;
+        self.register.e = (self.register.e >> 1) | carry;
+
+        self.register.set_flag(FlagId::Z, self.register.e == 0);
+        self.register.set_flag(FlagId::N, false);
+        self.register.set_flag(FlagId::H, false);
+        self.register.set_flag(FlagId::C, lsb != 0);
+
+        8
+    }
+
+    pub fn rr_h(&mut self) -> isize {
+        let lsb = self.register.h & 0x1;
+        let carry = (self.register.is_flag_set(FlagId::C) as u8) << 7;
+        self.register.h = (self.register.h >> 1) | carry;
+
+        self.register.set_flag(FlagId::Z, self.register.h == 0);
+        self.register.set_flag(FlagId::N, false);
+        self.register.set_flag(FlagId::H, false);
+        self.register.set_flag(FlagId::C, lsb != 0);
+
+        8
+    }
+
+    pub fn rr_l(&mut self) -> isize {
+        let lsb = self.register.l & 0x1;
+        let carry = (self.register.is_flag_set(FlagId::C) as u8) << 7;
+        self.register.l = (self.register.l >> 1) | carry;
+
+        self.register.set_flag(FlagId::Z, self.register.l == 0);
+        self.register.set_flag(FlagId::N, false);
+        self.register.set_flag(FlagId::H, false);
+        self.register.set_flag(FlagId::C, lsb != 0);
+
+        8
+    }
+
+    pub fn rr_hl(&mut self, mmu: &mut Mmu) -> isize {
+        let address = self.register.get_hl();
+        let old_value = mmu.read_byte(address);
+        let lsb = old_value & 0x1;
+        let carry = (self.register.is_flag_set(FlagId::C) as u8) << 7;
+        let new_value = (old_value >> 1) | carry;
+        mmu.write_byte(address, new_value);
+
+        self.register.set_flag(FlagId::Z, new_value == 0);
+        self.register.set_flag(FlagId::N, false);
+        self.register.set_flag(FlagId::H, false);
+        self.register.set_flag(FlagId::C, lsb != 0);
+
+        16
+    }
+
+    pub fn rr_a(&mut self) -> isize {
+        let lsb = self.register.a & 0x1;
+        let carry = (self.register.is_flag_set(FlagId::C) as u8) << 7;
+        self.register.a = (self.register.a >> 1) | carry;
+
+        self.register.set_flag(FlagId::Z, self.register.a == 0);
+        self.register.set_flag(FlagId::N, false);
+        self.register.set_flag(FlagId::H, false);
+        self.register.set_flag(FlagId::C, lsb != 0);
+
+        8
+    }
+
     pub fn rrc_a(&mut self) -> isize {
         let old_a = self.register.a;
         self.register.a = old_a.rotate_right(1);
